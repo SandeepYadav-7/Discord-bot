ca7f58d40fcb5f7f6e1033b80a8e697362be125f
Sandeep Yadav <81793952+SandeepYadav143@users.noreply.github.com>
2021-04-12 21:43:23 +0530

    Add files via upload

--- commands/gif.py
+++ commands/gif.py
@@ -9,7 +9,9 @@ class GIF(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    
+    @commands.command()
+    async def test(self, ctx):
+        await ctx.send("test is working!")
     
     
 
