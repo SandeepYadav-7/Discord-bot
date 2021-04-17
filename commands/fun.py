7d608a38a3de5106aa38a74e1502e28bdb0595fb
Sandeep Yadav <81793952+SandeepYadav143@users.noreply.github.com>
2021-04-17 16:05:58 +0530

    Add files via upload

--- commands/fun.py
+++ commands/fun.py
@@ -2,26 +2,30 @@ import discord
 from discord.ext import commands
 
 class Fun(commands.Cog):
-	def __init__(self, bot):
-		self.bot = bot
-	
-	
-	@commamds.command(name = "say", aliases = ["repeat"])
-	async def say(self, ctx, *, msg):
-		await ctx.send(msg)
-	
-	@say.error
-	async def say_error(self, ctx, error):
-		await ctx.send("You forgot to give me input to repeat!")
-	
-	@commands.command(name = "reply")
-	async def reply(self, ctx):
-		await ctx.message.add_reaction("✅")
-		message = await ctx.reply("I replied! (but didn't mention)\nThis command is just for testing (◍•ᴗ•◍)")
-		await message.add_reaction("😊")
-	
-	
-	
-
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command(
+        name = "say",
+        aliases = ["repeat"]
+        )
+    async def say(self, ctx, *, msg):
+        await ctx.send(msg)
+        await ctx.message.delete()
+    
+    @say.error
+    async def say_error(self, ctx, error):
+        await ctx.send("You forgot to give me input to repeat!")
+    
+    @commands.command(
+        name = "reply",
+        hidden = True
+        )
+    async def reply(self, ctx):
+        await ctx.message.add_reaction("✅")
+        msg = await ctx.reply("I replied! (but didn't mention\nThis command is just for testing (◍•ᴗ•◍)")
+        await msg.add_reaction("😊")
+    
+    
 def setup(bot):
-	bot.add_cog(Fun(bot))
+    bot.add_cog(Fun(bot)
