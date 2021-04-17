0a8c7d34fd891e7af4d6caf58e610ffe5c6eca0e
Sandeep Yadav <81793952+SandeepYadav143@users.noreply.github.com>
2021-04-17 15:58:05 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,52 +2,59 @@ import discord
 from discord.ext import commands
 import os
 from keep_alive import keep_alive
+from pretty_help import PrettyHelp # pip install discord-pretty-help
 
 intents = discord.Intents.default()
 intents.members = True
 
-prefix_list = ["N.", "n.", "Nezuko", "nezuko"]
-bot = commands.Bot(command_prefix = prefix_list, description = "Still Development", case_insensitive = True, intents = intents)
+prefix_list = ["K.", "k.", "kanao ", "kanao", "Kanao ", "Kanao"]
+bot = commands.Bot(
+    command_prefix = prefix_list,
+    case_insensitive = True,
+    strip_after_prefix = True,
+    intents = intents
+    # help_command = PrettyHelp()
+    )
 
-@bot.command()
+@bot.command(hidden = True)
 @commands.is_owner()
 async def load(ctx, extension):
-	bot.load_extension(f"commands.{extension}")
-	await ctx.send(f"✅ Successfully loaded {extension} cog.")
+    bot.load_extension(f"commands.{extension}")
+    await ctx.send(f"✅ Successfully loaded {extension} cog.")
 
 @load.error
 async def load_error(ctx, error):
-	await ctx.send(error)
+    await ctx.send(error)
 
-@bot.command()
+@bot.command(hidden = True)
 @commands.is_owner()
 async def unload(ctx, extension):
-	bot.unload_extension(f"commands.{extension}")
-	await ctx.send(f"✅ Successfully unloaded {extension} cog.")
+    bot.unload_extension(f"commands.{extension}")
+    await ctx.send(f"✅ Successfully unloaded {extension} cog.")
 
 @unload.error
 async def unload_error(ctx, error):
-	await ctx.send(error)
+    await ctx.send(error)
 
-@bot.command()
+@bot.command(hidden = True)
 @commands.is_owner()
 async def reload(ctx, extension):
-	bot.unload_extension(f"commands.{extension}")
-	await ctx.send(f"✅ Successfully unloaded {extension} cog.")
-	
-	bot.load_extension(f"commands.{extension}")
-	await ctx.send(f"✅ Successfully loaded {extension} cog.")
+    bot.unload_extension(f"commands.{extension}")
+    await ctx.send(f"✅ Successfully unloaded {extension} cog.")
+    
+    bot.load_extension(f"commands.{extension}")
+    await ctx.send(f"✅ Successfully loaded {extension} cog.
+    ")
 
 @reload.error
 async def reload_error(ctx, error):
-	await ctx.send(error)
-
-
+    await ctx.send(error)
 
 
 for filename in os.listdir("./commands"):
-	if filename.endswith(".py") and filename != "__init__.py":
-		bot.load_extension(f'commands.{filename[:-3]}')
+    if filename.endswith(".py") and filename != "__init__.py":
+        bot.load_extension(f"co
+
 
 keep_alive()
 bot.run(os.getenv("TOKEN"))
